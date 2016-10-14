21cda496002d538ff9d282fe13947c214b68f4d4
Markus Dangl <markus@q1cc.net>
2016-10-14 06:06:58 +0200

    Added mocha, chai & husky for testing.

---
+++ test/TestHash.js
@@ -0,0 +1,10 @@
+var expect = require("chai").expect
+  ;
+
+describe("Hash", function() {
+   describe(".hash()", function() {
+       it("should work the same as Hash()", function(){
+           
+       });
+   });
+});
